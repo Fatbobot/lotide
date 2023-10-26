3ffc7a1253482b43f2e88ba00958142649f4f9ed
Paul Jean <Fatbobot@gmail.com>
2023-10-26 19:48:49 -0300

    head function with asser equal combo

--- head.js
+++ head.js
@@ -14,3 +14,12 @@ const assertEqual = function(actual, expected) {
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1,1);
+
+//head function
+const head = function(arr) {
+  return(arr[0]);
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse, Labs"]),"Hello")
+assertEqual(head([]))
