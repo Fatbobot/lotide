bc43b51048c430a1a2a979287cab50a5e09aa6e7
Paul Jean <Fatbobot@gmail.com>
2023-10-26 18:50:03 -0300

    assert equal function being added to lotide project. Checks if input is expected

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log (`Assertion Passed: ${actual} == ${expected}`);
+    return true;
+
+  } else {
+    console.log (`Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+//Test code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
