d012ca74f8d73a8078ec7a9d7bc609bb831ff28e
Paul Jean <Fatbobot@gmail.com>
2023-11-01 22:56:40 -0300

    take until

---
+++ takeUntil.js
@@ -0,0 +1,17 @@
+const takeUntil = function(arr, callback) {
+  results = [];
+  for (const item of arr) {
+    if (!callback(item)) {
+
+    results.push(item);
+    }else {
+    break
+    }
+  }
+  return results;
+}
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
