79eeea4cc9088e08f00bd5529fbae65a75813ad0
Paul Jean <Fatbobot@gmail.com>
2023-11-11 01:31:59 -0400

    refactoring assertArraysEqual.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,3 +4,5 @@ const eqArrays = require('../eqArrays');
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
