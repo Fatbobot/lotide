039e600f5aa75bebb0dc37939e3d3d6d3f1452ba
Paul Jean <Fatbobot@gmail.com>
2023-11-11 03:01:22 -0400

    refactoring middleTest.js and tailTest.js to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,20 +1,21 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
-    assertEqual(head([1, 2, 3]), 1);
+    assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assertEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5'); 
   });
   it("returns '5' for ['5, 6, 7']", () => {
-    assertEqual(head([5,6,7]), 5); 
+    assert.strictEqual(head([5,6,7]), 5); 
   });
   it("returns 'Hello' for [Hello, Lightouse, Labs]", () => {
-    assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
   });
   it("returns 'undefined' for []", () => {
-    assertEqual(head([]), undefined); 
+    assert.strictEqual(head([]), undefined); 
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,16 @@
-
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail.js');
 
-assertArraysEqual(tail([5,6,7]), [6, 7]);
-assertArraysEqual(tail([5,6,7]).length, 2);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5,6,7]), [6, 7]);
+  });
+  it("returns '2' for [5,6,7]", () => {
+    assert.deepEqual(tail([5,6,7]).length, 2); 
+  });
+  it("returns '['Lighthouse', 'Labs']' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+});
+
