7ec8701079d9625b12a851adf724d6fa13e6f1bc
Paul Jean <Fatbobot@gmail.com>
2023-11-11 01:26:03 -0400

    refactoring eqArrays.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,7 @@
+
+const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail.js');
 
-tail([5,6,7]);
-tail(["Hello", "Lighthouse", "Labs"]);
-tail([]);
+assertArraysEqual(tail([5,6,7]), [6, 7]);
+assertArraysEqual(tail([5,6,7]).length, 2);
+assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
